68c9d017681c044cbea64b0492cfe924cdcce54a
Pascal Vautour <pascal.vautour@nrcan-rncan.gc.ca>
2022-12-20 09:37:15 -0500

    init

---
+++ index.js
@@ -0,0 +1,51 @@
+const { Client } = require("@opensearch-project/opensearch");
+const { defaultProvider } = require("@aws-sdk/credential-provider-node");
+const createAwsOpensearchConnector = require("aws-opensearch-connector");
+
+var host = 'https://search-meta-search-v4-6cloxdgo2wnscy6ohegxjkywwy.ca-central-1.es.amazonaws.com' // e.g. https://my-domain.region.es.amazonaws.com
+
+const getClient = async () => {
+    const awsCredentials = await defaultProvider()();
+    const connector = createAwsOpensearchConnector({
+        credentials: awsCredentials,
+        region: process.env.AWS_REGION ?? 'ca-central-1',
+        getCredentials: function(cb) {
+            return cb();
+        }
+    });
+    return new Client({
+        ...connector,
+        node: host,
+    });
+}
+
+async function search() {
+
+    // Initialize the client.
+    var client = await getClient();
+
+    // Create an index.
+    var index_name = "test-index";
+    var response = await client.indices.create({
+        index: index_name,
+    });
+
+    console.log("Creating index:");
+    console.log(response.body);
+
+    // Add a document to the index.
+    var document = {
+        "title": "Moneyball",
+        "director": "Bennett Miller",
+        "year": "2011"
+    };
+
+    var response = await client.index({
+        index: index_name,
+        body: document
+    });
+
+    console.log(response.body);
+}
+
+search().catch(console.log);
